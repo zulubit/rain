9eb8badc85af518bea9379d1728299db86ad5be1
zulubit <zaanfr@gmail.com>
2025-09-03 22:04:25 +0200

    feat: FOUC

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@
  * @fileoverview RainWC - Reactive Web Components with JSX
  */
 
+// Inject FOUC prevention CSS immediately when framework is imported
+if (typeof document !== 'undefined' && !document.querySelector('#rainwc-fouc-styles')) {
+  const style = document.createElement('style')
+  style.id = 'rainwc-fouc-styles'
+  style.textContent = `
+    /* RainWC FOUC Prevention - hides undefined custom elements */
+    :not(:defined) {
+      visibility: hidden;
+    }
+  `
+  document.head.appendChild(style)
+}
+
 // Core exports - reactive primitives
 export { $, css, render } from './core.js'
 
